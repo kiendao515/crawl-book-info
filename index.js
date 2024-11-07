cb712326855892c3394bc47544f0eb839372fe4a
Dao Trung Kien <kiendt120@ghtk.co>
2024-11-07 14:37:32 +0700

    update

--- index.js
+++ index.js
@@ -27,21 +27,45 @@ app.get('/api/book/:isbn', async (req, res) => {
         // Điều hướng đến URL của sách với ISBN
         await page.goto(url, { waitUntil: 'networkidle2' });
 
-        await page.waitForSelector('#aBibDetail727468');
-        await page.click('#aBibDetail727468');
+        const buttons = await page.$$('.btn.btn-default'); // Thay bằng class thực tế của button
+        for (const button of buttons) {
+            const buttonText = await page.evaluate(el => el.textContent, button);
+            if (buttonText.includes('Xem chi tiết')) {
+                await button.click();
+                break; 
+            }
+        }
 
-        // Chờ cho các phần tử cần thiết tải xong (sử dụng selector của phần tử mà bạn muốn lấy)
-        await page.waitForSelector('#divBibDetail727468 > div.media-body > dl > dd:nth-child(2) > dl > dd:nth-child(2) > a');
+        await page.waitForSelector('div.media-body > dl > dd:nth-child(2) > dl > dd:nth-child(2) > a');
 
         // Lấy dữ liệu từ trang sau khi đã tải đầy đủ
         const result = await page.evaluate(() => {
             const title = document.querySelector('#ulSearchResult > li > div.media-body > h4 > a')?.innerText.trim();
-            const author = document.querySelector('#divBibDetail727468 > div.media-body > dl > dd:nth-child(2) > dl > dd:nth-child(2) > a')?.innerText.trim();
-            const publisher = document.querySelector('#divBibDetail727468 > div.media-body > dl > dd:nth-child(3) > dl > dd:nth-child(2)')?.innerText.trim();
-            const description = document.querySelector('#divBibDetail727468 > div.media-body > dl > dd:nth-child(5) > dl > dd:nth-child(2)')?.innerText.trim();
-            const tags = document.querySelector('#divBibDetail727468 > div.media-body > dl > dd:nth-child(6) > dl > dd:nth-child(2) > a:nth-child(1)')?.innerText.trim()
-            return { title, author, publisher, description, tags };
+            const author = document.querySelector('div.media-body > dl > dd:nth-child(2) > dl > dd:nth-child(2) > a')?.innerText.trim();
+            let publisher = document.querySelector('div.media-body > dl > dd:nth-child(3) > dl > dd:nth-child(2)')?.innerText.trim();
+            const description = document.querySelector('div.media-body > dl > dd:nth-child(5) > dl > dd:nth-child(2)')?.innerText.trim();
+            const tags = document.querySelector('div.media-body > dl > dd:nth-child(6) > dl > dd:nth-child(2)')?.innerText.trim()
+            const yearRegex = /\b\d{4}\b$/;
+            const yearMatch = publisher.match(yearRegex);
+            const publisherRegex = /; (.*?), \d{4}$/;
+            const pageRegex = /(\d+)\s*tr/;
+            let year = '';
+            let pages;
+            const publisherMatch = publisher.match(publisherRegex);
+            if (publisherMatch) {
+                publisher = publisherMatch[1].trim();
+            }
+            if (yearMatch) {
+                year = yearMatch[0];
+                publisher = publisher.replace(/^H\.:.*?; /, '').replace(yearRegex, '').trim().replace(/[,;]$/, '');
+            }
+            const pageMatch = description.match(pageRegex);
+            if (pageMatch) {
+                pages = pageMatch[1];
+            }
+            return { title, author, publisher: publisher, publish_year: year, page: pages, tags };
         });
+
 
         // Đóng trình duyệt
         await browser.close();
